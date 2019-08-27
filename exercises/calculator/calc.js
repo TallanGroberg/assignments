2ff114f753bf5ca1a244cedb7b0d2996f69ada0c
tallan groberg <tallan.taven@gmail.com>
2019-08-27 16:11:31 -0600

    calc done

--- exercises/calculator/calc.js
+++ exercises/calculator/calc.js
@@ -1,41 +1,58 @@
-operators = ['add', 'subtract', 'divide', 'multiply']
-const addTo = document.getElementById('operator')
+operators = ['add', 'subtract', 'divide', 'multiply', 'modulate', 'square', 'cube' ]
 
 function opChoice() {
+  const addTo = document.getElementById('operator')
  operators.map(o => {
   
   const option = document.createElement('option')
   option.value = o
   option.textContent = o 
   addTo.appendChild(option)
+  console.log(option)
 })
  return addTo
 }
+console.log(opChoice())
+
 
 document.getElementById('submit').addEventListener('click', function(e) {
-  const first = document.getElementsByClassName('first')[0].value ;
+  const first = document.getElementsByClassName('first')[0].value;
+
+  const theOption = document.getElementById('operator').value
   
   const second = document.getElementsByClassName('second')[0].value
   
-  const both = [first, second]
+  
+  const answer = () => {
+    const both = [first, theOption, second]
+    console.log(both)
+    if (theOption === 'add') {
+       return parseInt(first) + parseInt(second) 
+    } else if (theOption === 'subtract') {
+      return parseInt(first) - parseInt(second)
+    } else if (theOption === 'multiply') {
+      return parseInt(first) * parseInt(second)
+    } else if (theOption === 'divide') {
+      return parseInt(first) / parseInt(second)
+    } else if (theOption === 'modulate') {
+      return parseInt(first) % parseInt(second)
+    } else if (theOption === 'modulate') {
+      return parseInt(first) % parseInt(second)
+    } else if (theOption === 'square') {
+      return parseInt(first) * parseInt(first) 
+    }
+    else if (theOption === 'cube') {
+      return parseInt(first) ** parseInt(first) 
+    }
+  }
+
+
   
   const output = document.getElementsByClassName('result')[0]
-  output.textContent = both
-  
+  output.innerHTML = answer()
 })
 
 function showResult() {
   const display = document.getElementsByClassName('result')[0]
-  //TODO make the input values show here
-}
-
-
-
-
-// show the input numbers in the result
   
-
-
-// if ( both ==  /^\d+$/) {
-  //   return both 
-  // }
+}

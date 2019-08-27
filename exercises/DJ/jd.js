f40a855e045cc285892ea7ce51a3536c0d839026
tallan groberg <tallan.taven@gmail.com>
2019-08-26 18:07:35 -0600

    dj.js

---
+++ exercises/DJ/jd.js
@@ -0,0 +1,26 @@
+
+ function blueify() {
+  // const blue = document.getElementsByClassName('container')
+  document.getElementsByClassName('container')[0].style.backgroundColor = "blue"
+}
+
+function redificate() {
+  const red = document.getElementsByClassName('container')[0]
+  red.style.backgroundColor = 'red'
+}
+
+function oldYeller() {
+  const yeller = document.getElementsByClassName('container')[0]
+  yeller.style.backgroundColor = "yellow"
+}
+
+function greenery() {
+  const greens = document.getElementsByClassName('container')[0]
+  greens.style.backgroundColor = "green"
+}
+
+function scrollyPolly() {
+  const olly = document.getElementsByClassName('container')[0]
+  olly.style.backgroundColor = "orange"
+}
+

cf0c152251a837122e79ce88740ff06506cb8561
tallan groberg <tallan.taven@gmail.com>
2019-09-01 12:47:25 -0600

    add travel form

--- exercises/travelForm/travel.js
+++ exercises/travelForm/travel.js
@@ -1,11 +1,72 @@
+
+const place = ['kentucky', 'virginia', 'vermont']
+const diet = ['vegitarian', 'kosher', 'lactose free']
+
+
+
+
+
+
+function spots() {
+  addTo = document.getElementById('daPlaces')
+  place.map( p => {
+    const option = document.createElement('option')
+    option.value = p
+    option.textContent = p 
+    addTo.appendChild(option)
+    
+    
+  })
+}
+console.log(spots())
+
 addEventListener('submit', function(e) {
   e.preventDefault()
-  const name = document.getElementById('name').value;
-  const lastName = document.getElementById('lastName').value;
-  const num = document.getElementById('age').value;
-  const gender = this.document.getElementById('gender').value
-  const result = document.getElementsByClassName('result').innerHTML = [name, lastName, num, gender]
-  console.log(result)
+  const form = document.form
+  const name = document.form.name.value;
+  const lastName = document.form.lastname.value;
+  const num = document.form.age.value;
+  const gender = document.form.gender.value
+  const thatPlace = document.form.place.value
+  const lac = document.form.lactose
+  const kosh = document.form.kosher 
+  const veg = document.form.veg
+  
+  const diet = [lac, kosh, veg ]
+  function eats() {
+   
+
+    for (let i = 0; i > diet.length; i++) {
+      
+
+      return i
+      // if ([i].checked === true) {
+      //   return [i].value 
+      // } else {
+      //   return null
+      // }
+    }
+ 
+
+  if (lac.checked === true) {
+    return lac.value
+    } else if (kosh.checked === true) {
+    return kosh.value
+    } else if (veg.checked === true) {
+    return veg.value
+    } else {
+      return null
+    }
+  }
+  
+  
+  
+  
+
+
+
+  const result = alert( [name, lastName, num, gender, thatPlace, eats()  ].join(', ') )
+  console.log(result,)
 })
 
 

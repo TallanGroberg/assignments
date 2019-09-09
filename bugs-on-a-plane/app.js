4d56edb009cc7073f4ecb9bb8d61d2d5af91fde7
tallan groberg <tallan.taven@gmail.com>
2019-09-09 11:03:51 -0600

    add JSON

--- bugs-on-a-plane/app.js
+++ bugs-on-a-plane/app.js
@@ -1,29 +1,50 @@
 
+// form.addEventListener('submit', function(e)  {
+//     e.preventDefault()
+//     var form = document.form;
+//     var name = form.firstName.value;
+//     var lastName = form.elements.lastName.value;
+//     var age = form.elements.age.value;
+//     var gender = form.elements.gender.value;
+//     var location = form.elements.location.value;
+//     var diet = [];
+//     let vegan = form.elements.vegan
+//     let gluten = form.elements.glutenFree
+//     let paleo = form.elements.paleo
+//         vegan.checked ? diet.push(vegan.value) : null
+//         gluten.checked ? diet.push(gluten.value) : null
+//         paleo.checked ? diet.push(paleo.value) : null
+//     alert(` \nFirst Name: ${name} \nLast Name:  ${lastName} \nAge: ${age} \nGender:  ${gender} \nTravel Location: ${location} \nDiet:
+//      ${diet} \nAwesome, now if you die, it won't be an accident..`);
+// })
 
 
-form.addEventListener('submit', function(e)  {
-    e.preventDefault()
+// // /^\d*[02468]$/ regrex for only even numbers
 
-    // console.log(e)
-    var form = document.form;
-    var name = form.firstName.value;
-    var lastName = form.elements.lastName.value;
-    var age = form.elements.age.value;
-    var gender = form.elements.gender.value;
-    var location = form.elements.location.value;
-    let vegan = form.elements.vegan
-    let gluten = form.elements.glutenFree
-    let paleo = form.elements.paleo
-    var diet = [];
-        vegan.checked ? diet.push(vegan.value) : null
-        gluten.checked ? diet.push(gluten.value) : null
-        paleo.checked ? diet.push(paleo.value) : null
-    console.log()
+// /^\*[0-9]$/;
+
+// function animals(chickens, cows, pigs) {
+// 	let chickenLeg = chickens * 2
+// 	let cowsLeg = cows * 4
+// 	let pigLeg = pigs * 4
+//     let result = [chickenLeg, cowsLeg, pigLeg]
+//     return result.reduce( (total, first) => first += total)
+// }
+    
+// console.log(animals(2, 3, 10))
+
+function addUp(num) {
+    let arr = []
+    for (let i = 0; i < num.length; i++) {
+        
+    }
     
     
     
-    alert("\nFirst Name: " + name + "\nLast Name: " + lastName + "\nAge: " + age + "\nGender: " + gender + "\nTravel Location: " + location + "\nDiet: " + diet + "\nAwesome, now if you die, it won't be an accident..");
-})
-    
+	
+	
+}
+
+console.log(addUp(9))
 
 
